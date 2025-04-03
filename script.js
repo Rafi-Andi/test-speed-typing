bf358ce78a8c4d2641becd491eaac5332a572fb2
M Rafi Andi Prayitno <rafiandiprayitno9528@gmail.com>
2025-04-03 21:03:40 +0700

    overflow

--- script.js
+++ script.js
@@ -62,5 +62,6 @@ const typingInit = () => {
     charSpan[charIndex].classList.add('aktif')
     salahValue.innerText = salah
 }
+
 randomParagraf()
 typingInput.addEventListener('input', typingInit)
